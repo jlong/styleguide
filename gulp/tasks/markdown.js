0086cbf61bc7eb207112fc505542cd3ef40d23f1
Michael Lee <michaelsoolee@gmail.com>
2018-02-15 16:54:02 -0500

    Swapped out markdown-it for showdown as markdown parser

--- gulp/tasks/markdown.js
+++ gulp/tasks/markdown.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import frontmatter from 'front-matter';
 // import pdc from 'pdc';
-import MarkdownIt from 'markdown-it';
+import showdown from 'showdown';
 import config from '../config';
 import reload from '../util/reload';
 import CodePreview from '../plugins/gulp-code-preview';
@@ -14,7 +14,14 @@ const $ = gulpLoadPlugins();
 
 
 gulp.task('markdown', ['layouts'], () => {
-  const markdown = new MarkdownIt({html: true});
+  const markdown = new showdown.Converter({
+    // Valid showdown options can be found:
+    // https://github.com/showdownjs/showdown#valid-options
+
+    // Provides GitHub style header IDs which are hyphenated when the header is
+    // separated by spaces
+    ghCompatibleHeaderId: true
+  })
   var building = process.env.build === 'true';
   var meta = { title: 'Style Guide' };
   var previews = new CodePreview(config.previews);
@@ -41,7 +48,7 @@ gulp.task('markdown', ['layouts'], () => {
       var content = frontmatter(String(file.contents));
       var filename = path.join(config.tmp.path('layouts'), (content.layout || 'main') + '.hb');
       var layout = fs.readFileSync(filename);
-      var result = markdown.render(content.body)
+      var result = markdown.makeHtml(content.body)
       content.attributes['contents'] = result;
       file.contents = new Buffer(layout);
       return content.attributes;
